f39af052bf47564fa0573e1e01f4a85d7489146c
Yuvi Masory <ymasory@gmail.com>
2010-09-26 15:01:34 -0400

    add start button with toast

--- src/com/github/ymasory/androidio/Main.java
+++ src/com/github/ymasory/androidio/Main.java
@@ -2,12 +2,38 @@ package com.github.ymasory.androidio;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	
+	TextView stats = null;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+//		setContentView(R.layout.main);
+		
+		Button startButton = new Button(this);
+		startButton.setText("START");
+		startButton.setEnabled(true);
+		
+		LinearLayout layout = new LinearLayout(this);
+		layout.addView(startButton);
+		
+		startButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				toast("HELLO");
+			}
+		});
+		
+		setContentView(layout);
+	}
+	
+	private void toast(String message) {
+		Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+	}
 }
